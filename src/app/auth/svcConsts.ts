9b84c9b0b2c4c6ba6a7be2f2009bbb76d8eda77a
smithbrianscott <smithbrianscott23@gmail.com>
2017-10-09 13:14:35 -0400

    Added bootstrap styles

--- src/app/auth/svcConsts.ts
+++ src/app/auth/svcConsts.ts
@@ -3,7 +3,7 @@ export class SvcConsts {
 	public static TENANT_ID:string = "{TENANT}.onmicrosoft.com";
 	public static GRAPH_RESOURCE:string = "https://graph.microsoft.com";
 	public static APP_SECRET:string = "APP_SECRET";
-	public static DRIVE_ID:string = "DRIVE_ID";
+	public static DRIVE_ID:string = "DRIVE_ID"; // This id is found by getting a list of drives (Document Libraries) on your sharepoint site. 
 
 	public static APP_TOKEN_URL:string = 'https://login.microsoftonline.com/{TENANT}.onmicrosoft.com/oauth2/token';
 
@@ -15,4 +15,5 @@ export class SvcConsts {
     // public static GRAPH_SITE_URL:string = 'https://graph.microsoft.com/v1.0/drives/{drive-id}/root/children';
     // public static GRAPH_UPLOAD_URL:string = 'https://graph.microsoft.com/v1.0/drives/{drive-id}/root:';
 	// public static GRAPH_FOLDER_URL:string = 'https://graph.microsoft.com/v1.0/drives/{drive-id}/root:/TicketID:/children';
+	// public static GRAPH_DRIVES_ON_SHAREPOINT_SITE: string = 'https://graph.microsoft.com/v1.0/sites/{TENENT}.sharepoint.com:{site-path(/sites/HR)}:/drives';
 }
